ebc8185e79c5075676169671f4a98be046aa2090
Alex <JaedaBeverly@Joelles-MacBook-Air.local>
2018-03-09 16:06:42 -0500

    This is the project start
    Project setup

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>My first React-Native App</Text>
+        <Text>This app will be an example for a different project</Text>
+        <Text>that I will be making later</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
