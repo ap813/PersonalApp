c0a65ff6a624f8269ef54c6c8e4d2a101bc58a7e
Alex <JaedaBeverly@Joelles-MacBook-Air.local>
2018-03-28 11:17:46 -0400

    Tutorial App Explosion
    Lynda tutorial App

--- App.js
+++ App.js
@@ -1,99 +1,12 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native'
 
-export default class App extends React.Component {
+import ColorList from './components/ColorList'
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      board : false,
-      signin : false
-    }
-
-    this.boards = this.boards.bind(this)
-    this.signup = this.signup.bind(this)
-  }
-
-  boards() {
-    this.setState({
-      board : true,
-      signin : false
-    })
-  }
-
-  signup() {
-    this.setState({
-      board : false,
-      signin : true
-    })
-  }
-
+export default class App extends Component {
   render() {
     return(
-      <View style={styles.holder}>
-        <View style={styles.head}>
-          <Text style={styles.header}>
-            Scout
-          </Text>
-          <Text style={styles.bodyText}>
-           {'\n'}
-            Welcome to Scout a discrete message board that is anonymous by nature.
-           {'\n'}
-           {'\n'}
-           {'\n'}
-           {'\n'}
-          </Text>
-          <Button
-            onClick={this.boards}
-            title="Look at the Boards"
-            color="black"
-            accessibilityLabel="Explore"
-          />
-          <Text>
-          {'\n'}
-          </Text>
-          <Button
-            onClick={this.signin}
-            title="SignIn to an Account"
-            color="black"
-            accessibilityLabel="Join the Team"
-          />
-        </View>
-      </View>
+      <ColorList />
     )
   }
 }
-
-const styles = StyleSheet.create({
-  holder: {
-    flex: 1,
-    backgroundColor: '#AED6F1',
-  },
-  head: {
-    flex: 2,
-  },
-  header: {
-    textAlign: 'center',
-    justifyContent: 'center',
-    color: "white",
-    fontSize: 42,
-    fontFamily: 'Cochin',
-    paddingTop: 15,
-    paddingBottom: 15,
-    backgroundColor: '#273746',
-  },
-  body: {
-    justifyContent: 'space-around',
-    flex: 3,
-    backgroundColor: '#AED6F1'
-  },
-  bodyText: {
-    textAlign: 'center',
-    color: 'white',
-    paddingTop: 30,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 30,
-    fontSize: 28,
-  },
-});
