9a3788dc2f9b3491fdbc0e801e96498ff68f71b3
Alex <JaedaBeverly@Joelles-MacBook-Air.local>
2018-03-29 15:32:18 -0400

    Bug fixes
    Fixed bugs with background colors

--- App.js
+++ App.js
@@ -1,12 +1,77 @@
 import React, { Component } from 'react'
-import { AppRegistry } from 'react-native'
+import {
+  AppRegistry,
+  StyleSheet,
+  ListView
+} from 'react-native'
 
-import ColorList from './components/ColorList'
+import ColorButton from './components/ColorButton'
+import ColorForm from './components/ColorForm'
 
-export default class App extends Component {
+export default class ColorList extends Component {
+
+  constructor() {
+    super()
+
+    this.ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
+    const availableColors = [
+      'red',
+      'green',
+      'yellow'
+    ]
+    this.state = {
+      backgroundColor: 'blue',
+      availableColors,
+      dataSource: this.ds.cloneWithRows(availableColors)
+    }
+    this.changeColor = this.changeColor.bind(this)
+    this.newColor = this.newColor.bind(this)
+  }
+
+  changeColor(backgroundColor) {
+    this.setState({backgroundColor})
+  }
+
+  newColor(color) {
+    const availableColors = [
+      ...this.state.availableColors,
+      color
+    ]
+    this.setState({
+      availableColors,
+      dataSource: this.ds.cloneWithRows(availableColors)
+    })
+  }
+
   render() {
-    return(
-      <ColorList />
+    const { backgroundColor, dataSource } = this.state
+    return (
+      <ListView style={[styles.container,{backgroundColor}]}
+        dataSource={dataSource}
+        renderRow={(color) => (
+          <ColorButton backgroundColor={color}
+            onSelect={this.changeColor}/>
+        )}
+        renderHeader={() => (
+          <ColorForm onNewColor={this.newColor}/>
+        )}>
+
+      </ListView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    backgroundColor: 'lightgrey',
+    padding: 10,
+    paddingTop: 20,
+    fontSize: 30,
+    textAlign: 'center'
+  }
+})
